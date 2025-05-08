e99a7b2cb016e2da0e04e1fb36e5e951c8e3c00e
Rafilde <rafasilva182003@gmail.com>
2025-05-08 17:04:38 -0300

    feat: normalizando os dados

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 
 data = np.loadtxt('aerogerador.dat', delimiter='\t')
 
-x = data[:,0].reshape(-1,1) # Velocidade do vento
-y = data[:,1].reshape(-1,1) # Potência do aerogerador
+speed = data[:,0].reshape(-1,1) # Velocidade do vento
+power = data[:,1].reshape(-1,1) # Potência do aerogerador
 
-# precisa normalizar ?
+# NORMALIZAÇÃO DOS DADOS ----------
 
-plt.scatter(x,y)
-plt.scatter(x, y, color='blue', label='Dados')
-plt.title('Gráfico de Dispersão: Velocidade do Vento e Potência Gerada')
-plt.xlabel('Velocidade do Vento')
-plt.ylabel('Potência Gerada')
+speed_min = speed.min()
+speed_max = speed.max()
+speed_norm = (speed - speed_min) / (speed_max - speed_min)
+
+power_min = power.min()
+power_max = power.max()
+power_norm = (power - power_min) / (power_max - power_min)
+
+# ---------------------------------
+
+plt.scatter(speed_norm, power_norm, color='blue', label='Dados Normalizados')
+plt.title('Velocidade do Vento vs Potência (Normalizado)')
+plt.xlabel('Velocidade Normalizada')
+plt.ylabel('Potência Normalizada')
 plt.legend()
-plt.grid(True)
 plt.show() # é não linear
